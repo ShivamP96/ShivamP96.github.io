2acf012fc08d3435739e528fe776838a785b1c8f
Shivam Patel <patelsd96@gmail.com>
2023-01-14 23:55:17 -0500

    changed result pdf object

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230113.003501';
+const cacheName = 'chirpy-20230114.235355';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
